4d7a16fa15c2c2db20fb90c38475f3e032a50045
Mohamedklby <Moha.klby333@gmail.com>
2025-12-10 17:31:23 +0100

    cyber

--- linux_security/0x00_linux_security_basics/5-audit_system.sh
+++ linux_security/0x00_linux_security_basics/5-audit_system.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash -c $'ss -tunap 2>/dev/null | head -n 20'
+bash -c $'last -F | head -n -5\nss -tanp 2>/dev/null\niptables -L -v 2>/dev/null\nif ss -lntup 2>/dev/null\nthen\n  netstat -tulpen\nfi'
