d40a9f6aa98eb3e7496fc6fec46f9df674d1e18e
mohamedklby <moha.klby333@gmail.com>
2026-02-19 17:37:08 +0100

    cyber

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# 0-sha1.sh
+# This script takes a password as an argument, hashes it using SHA-1,
+# and writes the resulting hash to 0_hash.txt
 
+# Check if a password is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <password>"
+  exit 1
+fi
+
+# Hash the password using SHA-1 and save only the hash (first column)
 echo -n "$1" | sha1sum | cut -d ' ' -f1 > 0_hash.txt
+
